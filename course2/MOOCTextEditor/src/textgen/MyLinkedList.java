bd8fd32f6f1d9d1779d375bec1806e0cc2263c26
RegularSvensson <elias.svensson.1992@gmail.com>
2016-11-13 16:28:12 +0100

    Implement LLNode() constructor

--- course2/MOOCTextEditor/src/textgen/MyLinkedList.java
+++ course2/MOOCTextEditor/src/textgen/MyLinkedList.java
@@ -141,14 +141,17 @@ class LLNode<E>
 	LLNode<E> next;
 	E data;
 
-	// TODO: Add any other methods you think are useful here
-	// E.g. you might want to add another constructor
-
 	public LLNode(E e) 
 	{
 		this.data = e;
 		this.prev = null;
 		this.next = null;
 	}
-
+	
+	public LLNode(E e, LLNode<E> next, LLNode<E> prev)
+	{
+		this.data = e;
+		this.prev = prev;
+		this.next = next; 
+	}
 }
